3ab6e6b131bab8c518e75e7a62f99687f6a6de7c
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-03 12:33:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     'I LOVE YOU TOO PUMPKIN!'
+  elseif phrase == phrase.upcase
+    'NO, NOT SINCE 1938!'
   else
-    puts 'HUH?! SPEAK UP, SONNY!'
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
